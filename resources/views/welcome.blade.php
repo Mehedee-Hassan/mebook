0263f96a3ada6f9281d713e96b173c6973a38429
Mehedee Hassan <razin.meha@gmail.com>
2017-02-25 01:30:05 +0600

    sign in ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,5 +6,51 @@
     @endsection
 
 @section('content')
-    this is body
+
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Sign Up</h3>
+
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Email </label>
+                    <input class="form-control" type="text" name="email" id="email"/>
+                </div>
+                <div class="form-group">
+                    <label for="Name">Name </label>
+                    <input class="form-control" type="text" name="Name" id="Name"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="Password">Password </label>
+                    <input class="form-control" type="password" name="Password" id="Password"/>
+                </div>
+
+                <input type="submit" class="btn btn-primary" value="submit"/>
+
+            </form>
+        </div>
+
+
+        <div class="col-md-6">
+<h3>Sign In</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Email </label>
+                    <input class="form-control" type="text" name="email" id="email"/>
+                </div>
+                
+
+                <div class="form-group">
+                    <label for="Password">Password </label>
+                    <input class="form-control" type="password" name="Password" id="Password"/>
+                </div>
+
+                <input type="submit" class="btn btn-primary" value="submit"/>
+
+            </form>
+        </div>
+    </div>
+
+
     @endsection
